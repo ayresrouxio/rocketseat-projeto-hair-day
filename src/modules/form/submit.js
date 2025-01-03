577bfaf2f00e126c4b1454ac9292cc992f9b488e
Ayres Roux <ayresrouxio@gmail.com>
2025-01-03 08:40:23 -0300

    aula: atualizando a lista de agendamentos

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,5 +1,6 @@
 import dayjs from "dayjs"
 import { scheduleNew } from "../../services/schedule-new"
+import { schedulesDay } from "../schedules/load"
 
 const form = document.querySelector("form")
 const clientName = document.querySelector("#client")
@@ -33,6 +34,10 @@ form.onsubmit = async (event) => {
     const id = new Date().getTime()
 
     await scheduleNew({ id, name, when })
+
+    await schedulesDay()
+
+    clientName.value = ""
   } catch (error) {
     alert("Não foi possível realizar o agendamento.")
     console.log(error)
